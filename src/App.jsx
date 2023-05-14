63e92eb47c1d270fd940026162bc321beffab76a
Prince Muhammad <princemuhammadofficial1@gmail.com>
2023-05-14 22:18:38 +0600

    Search fieled added!

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,10 @@ const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#2a804e] pt-10">
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#2a804e]">
         <Searchbar />
 
-        <div className="px-6 h-[calc(100vh-60px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
